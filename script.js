9988a696a689cc37d4bd38280ea4950385454920
Aryan <89633258+Aryan7585@users.noreply.github.com>
2022-03-25 18:55:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Programmer","Speedcuber","Science Enthusiast","Gamer"],
+        strings: ["Student", "Programmer","Speedcuber","Science Buff","Gamer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Programmer","Speedcuber","Science Enthusiast","Gamer"],
+        strings: ["Student", "Programmer","Speedcuber","Science Buff","Gamer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
